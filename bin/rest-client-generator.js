b7cb4984d62c51da3cd5d29ddbc87cee1304027c
Miroslav Dzurik <dzurik.miroslav@gmail.com>
2016-12-03 19:20:40 +0100

    cli interface, upgrade dependencies

---
+++ bin/rest-client-generator.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+'use strict';
+var restClientGenerator = require('../lib/rest-client-generator');
+var program = require('commander');
+
+program
+    .version('0.1.0')
+    .usage('[options] <file>')
+    .arguments('<file>')
+    .option('--output-file <value>', 'output file, default \'service.ts\'')
+    .option('--platform <value>', 'platform, yet only \'angular2-ts\'', /^(angular2-ts)$/i, 'angular2-ts')
+    .option('--root-url <value>', 'root URL of all REST calls')
+    .option('--default-service-name <value>', 'if first resource in WADL has method the servic name')
+    .option('--service-suffix <value>', 'service name suffix, default \'Service\'')
+    .option('--internal-variable-prefix <value>', 'internal variable prefix, default \'_\'')
+    .option('--no-capitalize', 'no capitalize resource and type names')
+    .parse(process.argv);
+
+    program.on('--help', function(){
+      console.log('  Examples:');
+      console.log('');
+      console.log('    $ rest-client-generator --output-file services.ts --root-url http://my.server/rest/ application.wadl');
+      console.log('    $ rest-client-generator --output-file services.ts http://my.server/rest/application.wadl');
+      console.log('');
+    });
+
+if (program.args.length !== 1) {
+    program.help();
+} else {
+    restClientGenerator({
+        wadlFile: program.args[0],
+        outputFile: program.outputFile,
+        platform: program.platform,
+        rootUrl: program.rootUrl,
+        defaultServiceName: program.defaultServiceName,
+        capitalize: program.capitalize,
+        serviceSuffix: program.serviceSuffix,
+        internalVariablePrefix: program.internalVariablePrefix
+    });
+}
--- test/index.js
+++ test/index.js
@@ -1,43 +1,47 @@
 var assert = require('assert');
 var fs = require('fs');
-var restClientGenerator = require('../');
-var parser = require('../wadl-parser');
+var rimraf = require('rimraf');
+var restClientGenerator = require('../lib/rest-client-generator');
+var wadlParser = require('../lib/wadl-parser');
+var exec = require('child_process');
 
 var outDir = __dirname + '/out';
 
 var opts = {
-  wadlFile: __dirname + '/application.wadl',
-  outputFile: outDir + '/service.ts',
-  platform: 'angular2-ts',
-  rootUrl: 'localhost:8080',
-  serviceSuffix: 'Service',
-  capitalize: true,
-  internalVariablePrefix: '_'
+    wadlFile: __dirname + '/application.wadl',
+    outputFile: outDir + '/service.ts',
+    platform: 'angular2-ts',
+    rootUrl: 'localhost:8080',
+    serviceSuffix: 'Service',
+    capitalize: true,
+    internalVariablePrefix: '_'
 }
 
-if (!fs.existsSync(outDir)) {
-  fs.mkdirSync(outDir);
-}
+rimraf.sync(outDir);
+fs.mkdirSync(outDir);
 
 describe('wadl-parser', function() {
-  it('should do something', function() {
-    var meta = parser(opts);
+    it('should do something', function() {
+        var meta = wadlParser(opts);
 
-    assert(meta);
-    assert(meta.types);
-    assert.equal(meta.rootUrl, opts.rootUrl);
-    assert(meta.services);
+        assert(meta);
+        assert(meta.types);
+        assert.equal(meta.rootUrl, opts.rootUrl);
+        assert(meta.services);
 
-    fs.writeFileSync(outDir + '/meta.json', JSON.stringify(meta, 1, 1), 'utf8');
-  });
+        fs.writeFileSync(outDir + '/meta.json', JSON.stringify(meta, 1, 1), 'utf8');
+    });
 });
 
 describe('wadl-client-generator', function() {
-  it('should do something', function() {
-    restClientGenerator(opts);
+    it('should do something', function() {
+        restClientGenerator(opts);
 
-    var serviceSrc = fs.readFileSync(opts.outputFile, 'utf8');
+        var serviceSrc = fs.readFileSync(opts.outputFile, 'utf8');
+        assert(serviceSrc);
 
-    assert(serviceSrc);
-  });
+        //try compile generated typescript
+        this.timeout(10000);
+        exec.execSync('tsc ' + opts.outputFile + ' --experimentalDecorators --moduleResolution node --baseUrl ./../node_modules/');
+    });
 });
