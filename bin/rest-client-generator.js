5e2debbce6e24b1f0c68c7a8edc15b4f819558c6
dzurikmiroslav <dzurik.miroslav@gmail.com>
2017-04-28 09:37:33 +0200

    Read WADL via HTTP

--- bin/rest-client-generator.js
+++ bin/rest-client-generator.js
@@ -6,8 +6,8 @@ var program = require('commander');
 
 program
     .version('0.1.0')
-    .usage('[options] <file>')
-    .arguments('<file>')
+    .usage('[options] <wadl>')
+    .arguments('<wadl>')
     .option('--output-file <value>', 'output file, default \'services.ts\'')
     .option('--platform <value>', 'platform, yet only \'angular2-ts\'', /^(angular2-ts)$/i, 'angular2-ts')
     .option('--root-url <value>', 'root URL of all REST calls')
@@ -18,19 +18,19 @@ program
     .option('--no-capitalize', 'no capitalize resource and type names')
     .parse(process.argv);
 
-    program.on('--help', function(){
-      console.log('  Examples:');
-      console.log('');
-      console.log('    $ rest-client-generator --output-file services.ts --root-url http://my.server/rest/ application.wadl');
-      console.log('    $ rest-client-generator --output-file services.ts http://my.server/rest/application.wadl');
-      console.log('');
-    });
+program.on('--help', function () {
+    console.log('  Examples:');
+    console.log('');
+    console.log('    $ rest-client-generator --output-file services.ts --root-url http://my.server/rest/ application.wadl');
+    console.log('    $ rest-client-generator --output-file services.ts http://my.server/rest/application.wadl');
+    console.log('');
+});
 
 if (program.args.length !== 1) {
     program.help();
 } else {
     restClientGenerator({
-        wadlFile: program.args[0],
+        wadl: program.args[0],
         outputFile: program.outputFile,
         platform: program.platform,
         rootUrl: program.rootUrl,
--- test/index.js
+++ test/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var assert = require('assert');
 var fs = require('fs');
 var rimraf = require('rimraf');
@@ -8,7 +10,7 @@ var exec = require('child_process');
 var outDir = __dirname + '/out';
 
 var opts = {
-    wadlFile: __dirname + '/application.wadl',
+    wadl: __dirname + '/application.wadl',
     outputFile: outDir + '/service.ts',
     platform: 'angular2-ts',
     rootUrl: 'localhost:8080',
@@ -20,28 +22,37 @@ var opts = {
 rimraf.sync(outDir);
 fs.mkdirSync(outDir);
 
-describe('wadl-parser', function() {
-    it('should do something', function() {
-        var meta = wadlParser(opts);
+describe('wadl-parser', function () {
+    it('should do something', function (done) {
+        wadlParser(opts).then(function (meta) {
+            assert(meta);
+            assert(meta.types);
+            assert.equal(meta.rootUrl, opts.rootUrl);
+            assert(meta.services);
 
-        assert(meta);
-        assert(meta.types);
-        assert.equal(meta.rootUrl, opts.rootUrl);
-        assert(meta.services);
+            fs.writeFileSync(outDir + '/meta.json', JSON.stringify(meta, 1, 1), 'utf8');
 
-        fs.writeFileSync(outDir + '/meta.json', JSON.stringify(meta, 1, 1), 'utf8');
+            done();
+        }, function (err) {
+            done(err);
+        });
     });
 });
 
-describe('wadl-client-generator', function() {
-    it('should do something', function() {
-        restClientGenerator(opts);
-
-        var serviceSrc = fs.readFileSync(opts.outputFile, 'utf8');
-        assert(serviceSrc);
-
-        //try compile generated typescript
+describe('wadl-client-generator', function () {
+    it('should do something', function (done) {
         this.timeout(10000);
-        exec.execSync('tsc ' + opts.outputFile + ' --experimentalDecorators --moduleResolution node --baseUrl ./../node_modules/ --target ES6');
+
+        restClientGenerator(opts).then(function () {
+            var serviceSrc = fs.readFileSync(opts.outputFile, 'utf8');
+            assert(serviceSrc);
+
+            //try compile generated typescript
+            exec.execSync('tsc ' + opts.outputFile + ' --experimentalDecorators --moduleResolution node --baseUrl ./../node_modules/ --target ES6');
+
+            done();
+        }, function (err) {
+            done(err);
+        });
     });
 });
